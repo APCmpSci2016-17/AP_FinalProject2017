196aa26d3de37b939079a1092f29941af530f5fa
Andrew Trobman <acemcbean1@gmail.com>
2017-05-18 11:19:25 -0400

    Full function implementation and decimal implementation

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -8,45 +8,58 @@ public class Evaluator {
 	{ 
 		//HashMap of all operations with corresponding precedence (higher takes place before lower)
 		//Note: Close Parentheses [')'] are not included here as it is handled uniquely
-		put(0,0); // ( Open Parentheses
-		put(1,1); // + Plus
-		put(2,1); // - Minus
-		put(3,2); // * Multiply
-		put(4,2); // / Divide
-		put(5,2); // % Modulus
-		put(6,4); // ^ Exponentiate
-		put(7,5); // ! Factorial
-		put(8,3); // - Unary Negation
-		put(9,0); // ( Function parenthesis
+		put(0,0);  // ( Open Parentheses
+		put(1,2);  // + Plus
+		put(2,2);  // - Minus
+		put(3,3);  // * Multiply
+		put(4,3);  // / Divide
+		put(5,3);  // % Modulus
+		put(6,5);  // ^ Exponentiate
+		put(7,6);  // ! Factorial
+		put(8,4);  // - Unary Negation
+		put(9,0);  // ( Function parenthesis
+		put(10,1); //, Argument separation
 	}};
 	
-	private static String binaries = "(+-*/%^! ("; //List of all binary operators and post-operators. Index important
-	private static String unaries  = "        - "; //List of all unary pre-operators. Parallel to the binaries list
+	private static String binaries   = " +-*/%^   ,"; //List of all binary operators and post-operators. Index important
+	private static String prefixes   = "(       -( ";
+	private static String postfixes  = "       !   "; //List of all unary pre-operators. Parallel to the binaries list
 	//Indices of these lists map to corresponding positions in the HashMap
 	
 	private static boolean isOp(char c) {
-		return binaries.indexOf(c) >= 0 || unaries.indexOf(c) >= 0;
+		return binaries.indexOf(c) >= 0 || prefixes.indexOf(c) >= 0 || postfixes.indexOf(c) >= 0;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println(stringEval("0+30/4*(1-3!)") + "\n");
-		System.out.println(stringEval("5*(3-1)") + "\n");
-		System.out.println(stringEval("5*3!") + "\n");
-		System.out.println(stringEval("5*(-3)") + "\n");
+//		System.out.println(stringEval("5*(3-1)") + "\n");
+//		System.out.println(stringEval("5*3!") + "\n");
+//		System.out.println(stringEval("5*(-3)") + "\n");
 		System.out.println(stringEval("5*-3") + "\n");
 		System.out.println(stringEval("5!*-3") + "\n");
-		System.out.println(stringEval("-3*5") + "\n");
-		System.out.println(stringEval("11!") + "\n");
-		System.out.println(stringEval("(((2+3))") + "\n"); //improper parentheses: should fail
-		System.out.println(stringEval("((2+3)))") + "\n"); //improper parentheses: should fail
-		System.out.println(stringEval("3/0+1") + "\n"); //Divide by zero: this case should fail
+//		System.out.println(stringEval("-3*5") + "\n");
+//		System.out.println(stringEval("11!") + "\n");
+//		System.out.println(stringEval("(((2+3))") + "\n"); //improper parentheses: should fail
+//		System.out.println(stringEval("((2+3)))") + "\n"); //improper parentheses: should fail
+//		System.out.println(stringEval("3/0+1") + "\n"); //Divide by zero: this case should fail
 		System.out.println(stringEval("4*double(4!-5)") + "\n");
+		System.out.println(stringEval("max(sin(6/5),cos(6/5))") + "\n");
+		System.out.println(stringEval("max(sin(1.2),cos(1.2))") + "\n");
 	}
 	
 	private static void applyFunc(String str, Stack<Double> num) {
 		switch (str) {
 		case "double":
 			num.push(num.pop()*2);
+			break;
+		case "sin":
+			num.push(Math.sin(num.pop()));
+			break;
+		case "cos":
+			num.push(Math.cos(num.pop()));
+			break;
+		case "max":
+			num.push(Math.max(num.pop(), num.pop()));
 			break;
 		}
 	}
@@ -55,7 +68,8 @@ public class Evaluator {
 		try { //yeah, yeah, I know. This is bad convention. Deal with it https://www.vignette3.wikia.nocookie.net/feed-the-beast/images/d/dd/Dealwithit.jpg/revision/latest?cb=20130809112807
 		Stack<Integer> sym = new Stack<>(); //Symbol stack
 		Stack<Double> num = new Stack<>(); //Number stack
-		Stack<String> fun = new Stack<>(); //Number stack
+		Stack<String> func = new Stack<>(); //Number stack
+		int decimals = -1;
 		double numBuff = 0; //Number buffer (for multiple digit numbers)
 		boolean numFlag = false; //True when current character read is a number
 		boolean stackFlag = false; //True when last stack pushed to was the number stack
@@ -66,6 +80,13 @@ public class Evaluator {
 		for (char c : ("(" + expr + ")").toCharArray()) { //Loop over the entire String
 //			System.out.print(c + " " + sym + " " + num); //Print out in format <current char> <symbol stack> <number stack>
 			if (c == ' ') { //do nothing if space
+			} else if (c == '.') {
+				if (!numFlag) {
+					numFlag = true;
+					numBuff = 0;
+				}
+				
+				decimals = 0;
 			} else if (Character.isAlphabetic(c)) {
 				if (strBuff == null) {
 					strBuff = Character.toString(c);
@@ -79,18 +100,21 @@ public class Evaluator {
 					numBuff = 0;
 				}
 				
+				if (decimals >= 0) decimals++;
 				numBuff *= 10; //Shift digits to the left i.e.: 101 -> 1010
 				numBuff += Character.digit(c, 10); //Add next digit i.e: 1010, 5 -> 1015
 			} else { //If the character is not a digit...
 				if (numFlag) { //If the last character read was a number
 					numFlag = false; //Reset the flag
+					if (decimals >= 0) numBuff /= Math.pow(10, decimals);
 					num.push(numBuff);
+					decimals = -1;
 					stackFlag = true; //Last stack modified was the number stack
 				}
 				
 				if (c == '(') { //If the current character is ( push 0 to the symbol stack and modify the flag
 					if (strBuff != null) {
-						fun.push(strBuff);
+						func.push(strBuff);
 						strBuff = null;
 						sym.push(9);
 					} else {
@@ -98,8 +122,21 @@ public class Evaluator {
 					}
 					stackFlag = false;
 				} else if (isOp(c) || c == ')') { //If the symbol is in the binary list or is )...
-					int cop = unaries.indexOf(c);
-					if (stackFlag || cop == -1) cop = binaries.indexOf(c);
+					int cop;
+					if (stackFlag) {
+						int bin = binaries.indexOf(c);
+						if (bin >= 0) {
+							cop = bin;
+							stackFlag = false;
+						} else {
+							cop = postfixes.indexOf(c);
+							stackFlag = true;
+						}
+					} else {
+						cop = prefixes.indexOf(c);
+						stackFlag = false;
+					}
+					
 					while (c == ')' || (!sym.isEmpty() && prec.get(sym.peek()) >= prec.get(cop))) {
 						/* if: the current symbol is ')'
 						 * 		collapse the stack until you reach the nearest '('
@@ -159,8 +196,10 @@ public class Evaluator {
 							num.push(-op1);
 							break;
 						case 9: // ( (function)
-							applyFunc(fun.pop(), num);
+							applyFunc(func.pop(), num);
 							stopEval = true;
+							break;
+						case 10: // ,
 							break;
 						}
 						
@@ -169,9 +208,7 @@ public class Evaluator {
 					}
 					if (c != ')') { //If the current character isn't ')'
 						sym.push(cop);
-						stackFlag = false; //last stack modified was the symbol stack
 					} else {
-						stackFlag = true; //special case, effectively anything in parentheses is a number therefore writing to the number stack
 					}
 				}
 			}
